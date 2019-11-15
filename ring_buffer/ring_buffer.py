a993880637e0f696e9224a80fb52924bf18b9ef5
Rory Murray <rory.christian.murray@gmail.com>
2019-11-15 11:51:03 -0500

    Get working, working on append

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,21 +5,45 @@ class RingBuffer:
         self.storage = [None]*capacity
 
     def append(self, item):
-        print(f"self.storage[0] bd : {self.storage[0]}")
+
+        # print(f"self.storage[0] bd : {self.storage[0]}")
         del self.storage[0]
-        print(f"self.storage[0] ad : {self.storage[0]}")
-        print(f"self.storage bi : {self.storage}")
+        # print(f"self.storage[0] ad : {self.storage[0]}")
+        # print(f"self.storage bi : {self.storage}")
         self.storage.append(item)
-        print(f"self.storage ai : {self.storage}")
+        # print(f"self.storage ai : {self.storage}")
 
     def get(self):
-        pass
+        temp = []
+        for i in self.storage:
+            if i is not None:
+                temp.append(i)
+        return temp
 
 
 rb1 = RingBuffer(5)
 
-rb1.append(1)
-# rb1.append(2)
-# rb1.append(3)
+rb1.append('a')
+rb1.append('b')
+rb1.append('c')
+rb1.append('d')
+print(len(rb1.storage))
+# len(rb1.storage)
+print("\n\n")
+print(rb1.get())
+print("\n\n")
+rb1.append('e')
+print("\n\n")
+print("\nlen(rb1.storage)\n")
+print(len(rb1.storage))
+print("\n\n")
+print("\nrb1.get()\n")
+print(rb1.get())
+print("\nrb1.append('f')\n")
+print(rb1.append('f'))
+print("\nlen(rb1.storage)\n")
+print(len(rb1.storage))
+print("\nrb1.get()\n")
+print(rb1.get())
 
-print(f"rb1 : {rb1}")
+# print(f"rb1 : {rb1}")
