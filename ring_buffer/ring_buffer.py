3d909263bc905f8f2af04392c12ddfb89c65292a
Rory Murray <rory.christian.murray@gmail.com>
2019-11-15 11:36:05 -0500

    Working on append

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,21 @@ class RingBuffer:
         self.storage = [None]*capacity
 
     def append(self, item):
-        pass
+        print(f"self.storage[0] bd : {self.storage[0]}")
+        del self.storage[0]
+        print(f"self.storage[0] ad : {self.storage[0]}")
+        print(f"self.storage bi : {self.storage}")
+        self.storage.append(item)
+        print(f"self.storage ai : {self.storage}")
 
     def get(self):
         pass
+
+
+rb1 = RingBuffer(5)
+
+rb1.append(1)
+# rb1.append(2)
+# rb1.append(3)
+
+print(f"rb1 : {rb1}")
